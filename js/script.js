926b1be85198b68be9f4d63ae4abd243d9e23335
noobthaibot <dgugik@ukr.net>
2021-10-26 10:58:04 +0300

    pushittothelimit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const userInfo = {
+    name: 'Roman',
+    surname: 'Lazurenko',
+    age: 26,
+    nation: 'Ukraine',
+    isMarried: false,
+    isBold: false,
+    isTall: false,
+    developmentExperience: null
+};
+alert('Hello!')
+console.log(userInfo.name + ' ' + userInfo.surname);
+console.log('Born in ' + userInfo.nation);
+console.log(userInfo.age + ' years old');
+console.log('Development experience: ' + userInfo.developmentExperience);
