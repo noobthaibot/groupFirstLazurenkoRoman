bd2692c0746e1bdd4c20c09b4cdbe7fd5b61e3cb
noobthaibot <88024655+noobthaibot@users.noreply.github.com>
2021-11-02 22:45:28 +0200

    push

--- js/homework_4_1.js
+++ js/homework_4_1.js
@@ -1,14 +1,13 @@
-const numbers = [5, 23, 43, 65, 76, 34, 3, 15, -3, 54, -12]
+const numbers = [5, 23, 43, 65, 76, 34, 3, 15, -3, 54, -12];
 
 let get_min = function (arr) {
     let min = arr[0];
     for (let i = 0; i < arr.length; i++) {
-      if (min > arr[i]) {
-        min = arr[i];
-      }
+        if (min > arr[i]) {
+            min = arr[i];
+        }
     }
     return min;
-  }
+};
 
 console.log(get_min(numbers));
-
--- js/homework_4_3.js
+++ js/homework_4_3.js
@@ -1,9 +1,9 @@
-function toReverseString (string) {
-    return string.split('').reverse().join('');
+function toReverseString(string) {
+    return string.split("").reverse().join("");
 }
 
-console.log(toReverseString('Sator'));
-console.log(toReverseString('Arepo'));
-console.log(toReverseString('Tenet'));
-console.log(toReverseString('Opera'));
-console.log(toReverseString('Rotas'));
+console.log(toReverseString("Sator"));
+console.log(toReverseString("Arepo"));
+console.log(toReverseString("Tenet"));
+console.log(toReverseString("Opera"));
+console.log(toReverseString("Rotas"));
