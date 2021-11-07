af92a3497cfe57aed6f355bbd4e2ebb99017f22e
noobthaibot <88024655+noobthaibot@users.noreply.github.com>
2021-11-07 15:54:05 +0200

    push

--- js/homework_5_4.js
+++ js/homework_5_4.js
@@ -1,21 +1,37 @@
-var points = {
-    "Вася": 200,
-    "Петя": 300,
-    "Даша": 250,
-    "Андрей": 150,
-    "Сергей": 450,
-    };
+let points = {
+    Вася: 200,
+    Петя: 300,
+    Даша: 250,
+    Андрей: 150,
+    Сергей: 450,
+};
 
-let VALUES = Object.values(points);
-console.log(VALUES);
-
-
+let VALUES = Object.entries(points);
 
 let totalSum = 0;
+let min = Infinity;
+let max = -Infinity;
+let minName = "";
+let maxName = "";
 for (const value of VALUES) {
-    console.log(value);
-    totalSum += value;
+    let name = value[0];
+    let point = value[1];
+    // console.log(name);
+    // console.log(point);
+    if (point > max) {
+        max = point;
+        maxName = name;
+    }
+    // console.log(maxName);
+
+    if (point < min) {
+        min = point;
+        minName = name;
+    }
+
+    totalSum += point;
 }
 
 console.log('Сумма всех оценок: ', totalSum);
-
+console.log("Минимальная оценка у", minName);
+console.log("Максимальная оценка у", maxName);
---
+++ js/homework_6_1.js
@@ -0,0 +1,24 @@
+const MS_SECOND = 1000;
+const MS_MINUTE = MS_SECOND * 60;
+const MS_HOUR = MS_MINUTE * 60;
+const MS_DAY = MS_HOUR * 24;
+
+
+function getCountWorkDays(date1, date2) {
+    firstDay = Date.parse(date1)
+    lastDay = Date.parse(date2)
+    // console.log(firstDay,lastDay);
+    if (lastDay < firstDay) {
+        return 0;
+    }
+    let totalDays = (lastDay - firstDay)/ MS_DAY;
+    // console.log(firstDay);
+    // console.log(lastDay);
+    let workDays = (totalDays / 7) * 5;
+    // console.log(workDays);
+    workDays = Math.floor(workDays)
+    // console.log(workDays);
+    return workDays;
+}
+
+console.log(getCountWorkDays('2021-08-11', '2021-10-01'));
