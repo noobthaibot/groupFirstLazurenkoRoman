cfaa57f8d5bef23b33f52dcb9808a20dc705f082
noobthaibot <dgugik@ukr.net>
2021-11-10 16:26:47 +0200

    push

--- js/homework_7.js
+++ js/homework_7.js
@@ -1,5 +1,6 @@
 function Calculator(name) {
     this.name = name;
+    this.date = new Date();
 }
 let calc = new Calculator("Мой калькулятор");
 
@@ -28,9 +29,17 @@ calc.getDivision = function (a, b) {
 };
 
 
+
+
+
 console.log("Сумма=" + calc.getSum(3, 54, 23, 53));
+console.log(calc.date);
 console.log("Результат умножения=" + calc.getMultiply(3, 5, 6, 5));
+console.log(calc.date);
 console.log("Результат вычетания=" + calc.getSubtraction(15, 3));
+console.log(calc.date);
 console.log("Остаток=" + calc.getDivision(144, 45));
+console.log(calc.date);
 // console.log(Calculator);
 // console.log(calc);
+
