8fd85ad1b68acf0e7dc78b0862b9068a087dac29
noobthaibot <dgugik@ukr.net>
2021-11-10 14:21:21 +0200

    push

--- js/homework_7.js
+++ js/homework_7.js
@@ -1,29 +1,36 @@
+function Calculator(name) {
+    this.name = name;
+}
+let calc = new Calculator("Мой калькулятор");
 
-function getSum() {
+calc.getSum = function () {
     let result = 0;
     for (let i = 0; i < arguments.length; i++) {
         result += arguments[i];
     }
     return result;
-}
+};
 
-function getMultiply() {
+calc.getMultiply = function () {
     let result = 1;
     for (let i = 0; i < arguments.length; i++) {
         result = arguments[i] * result;
     }
     return result;
-}
-function getSubtraction(a, b) {
+};
+calc.getSubtraction = function (a, b) {
     let result = a - b;
     return result;
-}
-function getDivision(a, b) {
+};
+calc.getDivision = function (a, b) {
     let result = a / b;
     return result;
-}
+};
 
-console.log("Сумма=" + getSum(3, 54, 23, 53));
-console.log("Результат умножения=" + getMultiply(3, 5, 6, 5));
-console.log("Результат вычетания=" + getSubtraction(15, 3));
-console.log("Остаток=" + getDivision(144, 45));
+
+console.log("Сумма=" + calc.getSum(3, 54, 23, 53));
+console.log("Результат умножения=" + calc.getMultiply(3, 5, 6, 5));
+console.log("Результат вычетания=" + calc.getSubtraction(15, 3));
+console.log("Остаток=" + calc.getDivision(144, 45));
+// console.log(Calculator);
+// console.log(calc);
