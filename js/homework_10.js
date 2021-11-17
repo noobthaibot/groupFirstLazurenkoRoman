fbb240f9c87c26c49b6d90829b5abf30995df7ed
noobthaibot <dgugik@ukr.net>
2021-11-17 10:48:28 +0200

    push

--- js/homework_10.js
+++ js/homework_10.js
@@ -1,63 +1,39 @@
 // $(document).ready(function () {
-//     const fetchPokemonById = (id, onSuccess, onError) => {
-//         fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-//             .then((result) => result.json())
-//             .then((pokemon) => {
-//                 onSuccess(pokemon);
-//             })
-//             .catch((error) => {
-//                 onError(error);
-//             });
+//     const fetchPost = (id) => {
+//         return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+//         .then((result) => result.json())
 //     };
 
-//     fetchPokemonById("pikachu", onFetchSuccess, onFetchError);
+//     fetchPost(2).then(onFetchSuccess).catch(onFetchError);
 
-//     function onFetchSuccess(pokemon) {
-//         console.log(pokemon);
-//     }
+//     function onFetchSuccess() {
+//         let json = fetchPost(2);
+//         console.log(JSON.stringify());
+//         for (let i = 0;i < json.lenght; i++) {
+//             let userObject = json[i];
+//             console.log(json[i]);
+//         }
+//     };
 //     function onFetchError(error) {
 //         console.log(error);
 //     }
-// });
 
+// });
 $(document).ready(function () {
-    const fetchPost = (id) => {
-        return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-        .then((result) => result.json())
-    };
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify({
+            title: "foo",
+            body: "bar",
+            userId: 1,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
 
-    fetchPost(2).then(onFetchSuccess).catch(onFetchError);
-
-    function onFetchSuccess() {
-        let json = fetchPost(2);
-        console.log(JSON.stringify());
-        for (let i = 0;i < json.lenght; i++) {
-            let userObject = json[i];
-            console.log(json[i]);
-            // let =  addUserViewToPage (userObject) 
-            //         for (let i = 0; i < 10; i++) {
-            //             $("body").append("<div></div>");
-            //             $("body div").addClass("atributes");
-            //             $('.atributes').text(userObject);
-            //         }
-            //     }
-                console.log(userObject);
-                return userObject;
-            // }
-        } 
-    };
-    function onFetchError(error) {
-        console.log(error);
-    }
-    
+    document.getElementById("name").innerHTML = response.name;
+    document.getElementById("job").innerHTML = response.job;
 });
-// $(document).ready(function () {
-//     fetch("https://jsonplaceholder.typicode.com/posts/1")
-//     .then((response) => {
-//         // console.log(response);
-//         return response.json();
-//     })
-//     .then((json) => console.log(json));
-
-    
-// });
